544cd8585bbc80dc425594bda82b62eafca3a160
Ludwig <ludwig.nachname.spam@gmail.com>
2023-01-01 14:49:49 +0100

    Removed Exception Handling for simplicity of project

--- src/com/mykingdomforapawn/refactoredportfolio/PortfolioInterface.java
+++ src/com/mykingdomforapawn/refactoredportfolio/PortfolioInterface.java
@@ -2,7 +2,7 @@ package com.mykingdomforapawn.refactoredportfolio;
 
 public interface PortfolioInterface {
     void openPosition(Asset asset);
-    void depositCash(int amount) throws InvalidInputException;
+    void depositCash(int amount);
     void withdrawCash(int amount);
     void displayPositions();
     void displayCash();
